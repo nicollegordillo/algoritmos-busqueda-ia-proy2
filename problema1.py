cee464750f34d5d1ea5a9de90eca949510631c12
MarchMol <marchmol98@gmail.com>
2025-04-24 19:04:31 -0600

    feat: prim algorithm

--- problema1.py
+++ problema1.py
@@ -2,6 +2,8 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import time
 
 class UnionFind:
     def __init__(self, M, N):
@@ -21,12 +23,97 @@ class UnionFind:
         return False
     
 def generate(M,N):
+    # prim(M,N, True)
+    kruskal(M,N,True)
+
+def get_walls(grid, x, y, M, N):
+    walls = []
+    if x<M-1:
+        if grid[x+1][y]>0:
+            walls.append(((x,y),(x+1,y)))
+    if y<N-1:
+        if grid[x][y+1]>0:
+            walls.append(((x,y),(x,y+1)))      
+    if x>0:
+        if grid[x-1][y]>0:
+            walls.append(((x,y),(x-1,y)))
+    if y>0:
+        if grid[x][y-1]>0:
+            walls.append(((x,y),(x,y-1)))
+    return walls
+
+def color_start(expanded, x, y):
+    for i in range(-1,2):
+        for j in range(-1,2):   
+            expanded[x+i][y+j] = 3
+def prim(M,N,animate):
+    grid = np.ones((M,N))
+    expanded = np.ones((2*M+1,2*N+1))
+    colors = ['white','black','red','blue']
+    cmap = ListedColormap(colors)
+    start = (random.randint(0,M-1),random.randint(0,N-1))
+    # start = (0,0)
+    grid[start[0]][start[1]] = 0
+    walls = get_walls(grid, start[0],start[1],M, N)
     
-    grid = np.zeros((M,N))
-    kruskal(M,N)
     
+    if animate:
+        plt.ion()  # Turn on interactive mode
+        fig, ax = plt.subplots()
+        ax.set_title("Prim Algorithm - Animated")
+        img = ax.imshow(expanded, cmap=cmap, vmin=0, vmax=3)
+        plt.axis('off')
     
-def kruskal(M, N):
+    # for i in range(4):
+    while len(walls)>0:
+        for w in walls:
+            if grid[w[1][0]][w[1][1]] > 0:
+                expanded[2*w[1][0]+1][2*w[1][1]+1] = 2
+        
+        rand_wall = walls.pop(random.randint(0,len(walls)-1))
+        
+        if grid[rand_wall[1][0]][rand_wall[1][1]]>0:
+
+            # Mark cell as visited
+            grid[rand_wall[0][0]][rand_wall[0][1]]=0
+            grid[rand_wall[1][0]][rand_wall[1][1]]=0
+            expanded[2*rand_wall[1][0]+1][2*rand_wall[1][1]+1] = 0
+
+            diff = (
+                rand_wall[1][0] - rand_wall[0][0],
+                rand_wall[1][1] - rand_wall[0][1]
+            )
+            expanded[
+                2*rand_wall[0][0] + diff[0] + 1,
+                2*rand_wall[0][1] + diff[1] + 1
+            ] = 0
+            
+            possible_walls = get_walls(
+                grid,rand_wall[1][0],
+                rand_wall[1][1],
+                M,N
+            )
+            for w in possible_walls:
+                if w not in walls:
+                    walls.append(w) 
+            color_start(expanded, 2*start[0]+1, 2*start[1]+1)
+            if animate:
+                img.set_data(expanded)
+                plt.draw()
+                plt.pause(0.01)
+    print("Finished!!")
+
+    if not animate:
+        fig, ax = plt.subplots()
+        img = ax.imshow(expanded, cmap=cmap, vmin=0,vmax=3)
+        plt.axis('off')
+    ax.set_title("Prim Algorithm - Finished!")
+    plt.ioff()
+    plt.show()
+        
+        
+    
+def kruskal(M, N, animate):
     expanded = np.ones((2*M + 1,2*N+1))
     for m in range(M):
         for n in range(N):
@@ -44,6 +131,12 @@ def kruskal(M, N):
     random.shuffle(walls)
     uf = UnionFind(M, N)
 
+
+    if animate:
+        plt.ion()  # Turn on interactive mode
+        fig, ax = plt.subplots()
+        img = ax.imshow(expanded, cmap='Greys', interpolation='nearest')
+        plt.axis('off')
     for cell1, cell2 in walls:
         if uf.union(cell1, cell2):
             # Determine the wall between cell1 and cell2 and remove it
@@ -52,9 +145,16 @@ def kruskal(M, N):
             wx = x1 + x2 + 1
             wy = y1 + y2 + 1
             expanded[wx][wy] = 0  # Remove wall
+            if animate:
+                img.set_data(expanded)
+                plt.draw()
+                plt.pause(0.00001)
     
-    plt.imshow(expanded, cmap='Greys', interpolation='nearest')
-    plt.axis('off')  # Turn off axes
+    if not animate:
+        fig, ax = plt.subplots()
+        img = ax.imshow(expanded, cmap='Greys', interpolation='nearest')
+        plt.axis('off')
+    plt.ioff()
     plt.show()
     
-generate(60,80)
+generate(40,40)
